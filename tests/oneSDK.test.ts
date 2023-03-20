4e22d81de3f9bfff4662151b09957c7aa493c071
Tasos Kaseris <kasseristas@gmail.com>
2023-03-20 21:40:09 +0100

    adding test group for single movie by id

--- tests/oneSDK.test.ts
+++ tests/oneSDK.test.ts
@@ -2,6 +2,7 @@ import initOneSDK from '../src';
 
 const oneSDK = initOneSDK(process.env.ONE_API_TOKEN)
 
+// Test group for /movie endpoint
 describe("all movies", () => {
     it("get all movies", async () => {
         const movies = await oneSDK.movies().getAll();
@@ -26,5 +27,29 @@ describe("all movies", () => {
         const movies = await oneSDK.movies().with("budgetInMillions", "<", 200).getAll();
     
         expect(movies).toHaveLength(3);
+    });
+});
+
+// Test group for /movie/{id endpoint}
+describe("one movie by id", () => {
+    it("get movie, name check", async () => {
+        const movie = await oneSDK.movie("5cd95395de30eff6ebccde5d").get();
+    
+        expect(movie).not.toBeNull();
+        expect(movie.name).toBe("The Return of the King");
+    });
+
+    it("get movie, budget check", async () => {
+        const movie = await oneSDK.movie("5cd95395de30eff6ebccde5d").get();
+    
+        expect(movie).not.toBeNull();
+        expect(movie.budgetInMillions).toBe(94);
+    });
+
+    it("get movie, budget check", async () => {
+        const movie = await oneSDK.movie("5cd95395de30eff6ebccde5d").get();
+    
+        expect(movie).not.toBeNull();
+        expect(movie.academyAwardWins).toBe(11);
     });
 });
