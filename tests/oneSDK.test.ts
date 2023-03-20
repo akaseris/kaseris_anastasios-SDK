d29179b040c75884a9bd6bde61d966d4b268bf34
Tasos Kaseris <kasseristas@gmail.com>
2023-03-20 21:30:04 +0100

    add tests for all movies endpoint

--- tests/oneSDK.test.ts
+++ tests/oneSDK.test.ts
@@ -1,2 +1,30 @@
-import oneSDK from '../src';
+import initOneSDK from '../src';
 
+const oneSDK = initOneSDK(process.env.ONE_API_TOKEN)
+
+describe("all movies", () => {
+    it("get movies", async () => {
+        const movies = await oneSDK.movies().getAll();
+    
+        expect(movies).toHaveLength(8);
+    });
+
+    it("get movies", async () => {
+        const movies = await oneSDK.movies().offset(3).getAll();
+    
+        expect(movies).toHaveLength(5);
+    });
+
+    it("get movies", async () => {
+        const movies = await oneSDK.movies().sort("name", "asc").getAll();
+    
+        expect(movies[0]).not.toBeNull();
+        expect(movies[0].name).toBe("The Battle of the Five Armies");
+    });
+
+    it("get movies", async () => {
+        const movies = await oneSDK.movies().with("budgetInMillions", "<", 200).getAll();
+    
+        expect(movies).toHaveLength(3);
+    });
+});
